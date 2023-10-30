69aaf9c3e615df6fb050e22fe71a9b997a534b60
Vaxim08 <vaxim.edengarde@gmail.com>
2023-10-30 23:25:59 +0100

    master

--- TP2/exercice_3.py
+++ TP2/exercice_3.py
@@ -6,19 +6,16 @@ n = i = 0
 while(n != x and i < 7):
     i = i + 1
     n = int(input("Donner un nombre (1 à 100): "))
-    if(n < 1 or n > 100):
+    if n < 1 or n > 100:
         print("Oups, vous avez saisi un nombre en dehors de l'intervalle")
-        continue
-    elif(n > x):
+    elif n > x:
         print("Oups, entrez un nombre plus petit")
-        continue
-    elif(n < x):
+    elif n < x:
         print("Oups, entrer un nombre plus grand")
-        continue
     else:
         print("Bravo",n,"est le nombre que j'ai choisit")
         print("Vous l'avez deviné",i,"essais")
-        continue
+        break
 
 if i == 7 :
     print("J'ai gagné, je suis le meilleur, Le nombre que j'ai deviné est",x)
